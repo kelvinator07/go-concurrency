d4ad4e980499bbeb37963a46f9c67a82aac02f7e
Kelvin Isievwore <kelvinator4leo@gmail.com>
2024-05-02 14:23:29 +0100

    Philosophers Dining Problem

---
+++ dining-philosophers/main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Philosopher struct {
+	name      string
+	leftFork  int
+	rightFork int
+}
+
+var philosophers = []Philosopher{
+	{name: "Plato", leftFork: 4, rightFork: 0},
+	{name: "Socrates", leftFork: 0, rightFork: 1},
+	{name: "Aristotle", leftFork: 1, rightFork: 2},
+	{name: "Pascal", leftFork: 2, rightFork: 3},
+	{name: "Lock", leftFork: 3, rightFork: 4},
+}
+
+var hunger = 3
+var eatTime = 1 * time.Second
+var thinkTime = 3 * time.Second
+var sleepTime = 1 * time.Second
+
+var orderMutex sync.Mutex
+var orderFinished []string
+
+func main() {
+	fmt.Println("Dining Philosopher Problem")
+	fmt.Println("--------------------------")
+
+	time.Sleep(sleepTime)
+
+	dine()
+
+	fmt.Println("The table is empty")
+
+	time.Sleep(sleepTime)
+	fmt.Println("Order finished")
+
+}
+
+func dine() {
+	// to run the program faster without delay
+	eatTime = 0 * time.Second
+	thinkTime = 0 * time.Second
+	sleepTime = 0 * time.Second
+
+	wg := &sync.WaitGroup{}
+	wg.Add(len(philosophers))
+
+	seated := &sync.WaitGroup{}
+	seated.Add(len(philosophers))
+
+	// forks is a map of all 5 forks
+	var forks = make(map[int]*sync.Mutex)
+	for i := 0; i < len(philosophers); i++ {
+		forks[i] = &sync.Mutex{}
+	}
+
+	// Start the meal by iterating through our slice of Philosophers
+	for i := 0; i < len(philosophers); i++ {
+		// go routine for each philosopher
+		go diningProblem(philosophers[i], wg, forks, seated)
+	}
+
+	// Wait for the philosophers to finish. This blocks until the wait group is 0.
+	wg.Wait()
+
+}
+
+// diningProblem is the function fired off as a gouroutine for each of our philosophers.
+func diningProblem(philosopher Philosopher, wg *sync.WaitGroup, forks map[int]*sync.Mutex, seated *sync.WaitGroup) {
+	defer wg.Done()
+
+	fmt.Printf("%s is seated at the table.\n", philosopher.name)
+
+	// decrement seated waitGroup by one
+	seated.Done()
+
+	// Wait until everyone is seated
+	seated.Wait()
+
+	for i := hunger; i > 0; i-- {
+		if philosopher.leftFork > philosopher.rightFork {
+			forks[philosopher.rightFork].Lock()
+			fmt.Printf("\t%s takes the right fork.\n", philosopher.name)
+			forks[philosopher.leftFork].Lock()
+			fmt.Printf("\t%s takes the left fork.\n", philosopher.name)
+		} else {
+			forks[philosopher.leftFork].Lock()
+			fmt.Printf("\t%s takes the left fork.\n", philosopher.name)
+			forks[philosopher.rightFork].Lock()
+			fmt.Printf("\t%s takes the right fork.\n", philosopher.name)
+		}
+
+		fmt.Printf("\t%s has both forks and is eating.\n", philosopher.name)
+		time.Sleep(eatTime)
+
+		fmt.Printf("\t%s is thinking.\n", philosopher.name)
+		time.Sleep(thinkTime)
+
+		// Unlock the mutexes for both forks
+		forks[philosopher.rightFork].Unlock()
+		forks[philosopher.leftFork].Unlock()
+
+		fmt.Printf("\t%s puts down the forks.\n", philosopher.name)
+	}
+
+	fmt.Println(philosopher.name, "is satisfied.")
+	fmt.Println(philosopher.name, "left the table.")
+
+	orderMutex.Lock()
+	orderFinished = append(orderFinished, philosopher.name)
+	orderMutex.Unlock()
+}
---
+++ dining-philosophers/main_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func Test_dine(t *testing.T) {
+	eatTime = 0 * time.Second
+	thinkTime = 0 * time.Second
+	sleepTime = 0 * time.Second
+
+	for i := 0; i < 10; i++ {
+		// Arrange
+		orderFinished = []string{} // reset every time
+
+		// Act
+		dine()
+
+		// Assert
+		if len(orderFinished) != 5 {
+			t.Errorf("incorrect length of slice; expected 5 but got %d", len(orderFinished))
+		}
+	}
+}
+
+func Test_dineWithVaryingDelays(t *testing.T) {
+	var tests = []struct {
+		name  string
+		delay time.Duration
+	}{
+		{"zero delay", time.Second * 0},
+		{"quarter second delay", time.Millisecond * 250},
+		{"half second delay", time.Millisecond * 500},
+	}
+
+	for _, e := range tests {
+		orderFinished = []string{} // reset every time
+
+		eatTime = e.delay
+		sleepTime = e.delay
+		thinkTime = e.delay
+
+		dine()
+		// Assert
+		if len(orderFinished) != 5 {
+			t.Errorf("%s: incorrect length of slice; expected 5 but got %d", e.name, len(orderFinished))
+		}
+	}
+}
